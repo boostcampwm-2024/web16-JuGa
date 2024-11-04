a982310c8d753d27d9d9f4269b1f3a5a47c919f0
JIN <o3ouuu@inu.ac.kr>
2024-11-04 21:11:50 +0900

    ⚙️ chore: BE 프로젝트 기본 세팅

---
+++ BE/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist'],
+  rules: {
+    '@typescript-eslint/lines-between-class-members': ['error', 'always', {
+      exceptAfterSingleLine: true,
+    }],
+    '@typescript-eslint/no-throw-literal': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-unsafe-argument': 'error',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off'
+  },
+};
