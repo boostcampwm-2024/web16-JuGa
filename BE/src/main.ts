a3651094b8b22f56bdf5f3b5748241e6e554d176
jinddings <pjm2476@gmail.com>
2024-11-05 18:30:43 +0900

    ✨ feat: swagger 설정 추가

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
---
+++ BE/src/util/swagger.ts
@@ -0,0 +1,20 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+/**
+ * Swagger 설정
+ *
+ * @param {INestApplication} app
+ *
+ */
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('Juga API')
+    .setDescription('Juga API 문서입니다.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
