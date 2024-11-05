f1f543a5f02cb558bfdc439c27b0e4738b3c3045
dongree <dongwoo0307@naver.com>
2024-11-05 23:35:44 +0900

    ✨ feat: 로그인 레이아웃 구성 #1

--- FE/src/page/Home.tsx
+++ FE/src/page/Home.tsx
@@ -1,14 +1,16 @@
-import Header from "components/Header";
-import TopFive from "components/TopFive/TopFive";
+import Header from 'components/Header';
+import Login from 'components/Login';
+import TopFive from 'components/TopFive/TopFive';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className={"pt-[60px]"}>
+      <div className={'pt-[60px]'}>
         <div>지수 정보들</div>
         <TopFive />
       </div>
+      <Login />
     </>
   );
 }
