f29a7bee54171b297b064c221b3c7ffbfc1e3c7d
anjdydhody <a01066444534@gmail.com>
2024-11-05 16:22:17 +0900

    ⚙️ chore: DB 연결 설정 수정

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql', // 데이터베이스 타입
       host: process.env.DB_HOST,
--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
