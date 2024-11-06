eee81012f5f2ba13e2e23771ebc8ae91d0965a58
jinddings <pjm2476@gmail.com>
2024-11-06 17:16:45 +0900

    ⚙️ chore:lint 실행시 발생하는 오류 수정

--- BE/src/auth/auth.controller.spec.ts
+++ BE/src/auth/auth.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserRepository } from './user.repository';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +9,17 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: UserRepository,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- BE/src/auth/auth.service.spec.ts
+++ BE/src/auth/auth.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserRepository } from './user.repository';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: UserRepository,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
