aa1fcab3f2a4c5f9609eaed734e038f064dcb310
dongree <dongwoo0307@naver.com>
2024-11-11 17:53:36 +0900

    💄 design: stock detail 페이지 간단한 레이아웃 구조 잡기 #60

---
+++ FE/src/components/StocksDetail/TradeSection.tsx
@@ -0,0 +1,3 @@
+export default function TradeSection() {
+  return <div className='w-full bg-purple-300 min-w-72'>주식 거래 창</div>;
+}
--- FE/src/page/StocksDetail.tsx
+++ FE/src/page/StocksDetail.tsx
@@ -1,3 +1,19 @@
+import Chart from 'components/StocksDetail/Chart';
+import Header from 'components/StocksDetail/Header';
+import PriceSection from 'components/StocksDetail/PriceSection';
+import TradeSection from 'components/StocksDetail/TradeSection';
+
 export default function StocksDetail() {
-  return <>stockindex</>;
+  return (
+    <div className='flex flex-col bg-red-50'>
+      <Header />
+      <div className='flex h-[500px] bg-yellow-600'>
+        <div className='flex min-w-[850px] flex-col'>
+          <Chart />
+          <PriceSection />
+        </div>
+        <TradeSection />
+      </div>
+    </div>
+  );
 }
