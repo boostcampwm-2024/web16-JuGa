5b8d2134bd4eda040f5b4514a52d05418dc20cd2
Seo San <dannysir@naver.com>
2024-11-11 18:38:02 +0900

    ✨ feat: 검색 모달 초기 레이아웃 생성 #62

--- FE/src/components/Search/index.tsx
+++ FE/src/components/Search/index.tsx
@@ -1,15 +1,36 @@
-import useSearchModalStore from '../../store/useSearchModalStore.ts';
-import Overay from '../../utils/ModalOveray.tsx';
+import { useState, useEffect } from 'react';
+import useSearchModalStore from '../../store/useSearchModalStore';
+import Overlay from '../../utils/ModalOveray';
+import { SearchInput } from './SearchInput';
+import { SearchHistoryList } from './SearchHistoryList';
 
 export default function SearchModal() {
   const { isOpen, toggleSearchModal } = useSearchModalStore();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSearchHistory(['서산증권', '삼성화재', '삼성전기']);
+  }, []);
+
+  const handleDeleteHistoryItem = (item: string) => {
+    setSearchHistory((prev) => prev.filter((history) => history !== item));
+  };
 
   if (!isOpen) return;
 
   return (
     <>
-      <Overay onClick={() => toggleSearchModal()} />
-      <section className='fixed left-1/2 top-[90px] flex w-[640px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-2xl bg-white p-20 shadow-lg'></section>
+      <Overlay onClick={() => toggleSearchModal()} />
+      <section className='fixed left-1/2 top-3 flex w-[640px] -translate-x-1/2 flex-col rounded-2xl bg-white shadow-lg'>
+        <div className={'flex flex-col gap-5 p-3'}>
+          <SearchInput value={searchTerm} onChange={setSearchTerm} />
+          <SearchHistoryList
+            searchHistory={searchHistory}
+            onDeleteItem={handleDeleteHistoryItem}
+          />
+        </div>
+      </section>
     </>
   );
 }
