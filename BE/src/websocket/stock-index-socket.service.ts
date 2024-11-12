437f622460e73665e6907f5e5be029f413e68641
anjdydhody <a01066444534@gmail.com>
2024-11-12 20:11:37 +0900

    ⚙️ chore: TR_ID로 변수명 수정

--- BE/src/websocket/stock-index-socket.service.ts
+++ BE/src/websocket/stock-index-socket.service.ts
@@ -5,7 +5,7 @@ import { SocketGateway } from './socket.gateway';
 
 @Injectable()
 export class StockIndexSocketService {
-  private TRADE_CODE = 'H0UPCNT0';
+  private TR_ID = 'H0UPCNT0';
   private STOCK_CODE = {
     '0001': 'KOSPI',
     '1001': 'KOSDAQ',
@@ -18,14 +18,14 @@ export class StockIndexSocketService {
     private readonly baseSocketService: BaseSocketService,
   ) {
     baseSocketService.registerSocketOpenHandler(() => {
-      this.baseSocketService.registerCode(this.TRADE_CODE, '0001'); // 코스피
-      this.baseSocketService.registerCode(this.TRADE_CODE, '1001'); // 코스닥
-      this.baseSocketService.registerCode(this.TRADE_CODE, '2001'); // 코스피200
-      this.baseSocketService.registerCode(this.TRADE_CODE, '3003'); // KSQ150
+      this.baseSocketService.registerCode(this.TR_ID, '0001'); // 코스피
+      this.baseSocketService.registerCode(this.TR_ID, '1001'); // 코스닥
+      this.baseSocketService.registerCode(this.TR_ID, '2001'); // 코스피200
+      this.baseSocketService.registerCode(this.TR_ID, '3003'); // KSQ150
     });
 
     baseSocketService.registerSocketDataHandler(
-      this.TRADE_CODE,
+      this.TR_ID,
       (data: string[]) => {
         this.socketGateway.sendStockIndexValueToClient(
           this.STOCK_CODE[data[0]],
