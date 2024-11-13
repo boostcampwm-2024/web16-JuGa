3c650ac984e8df627e76a9da63f9491b3b610eac
JIN <o3ouuu@inu.ac.kr>
2024-11-13 19:46:15 +0900

    ♻️ refactor: requestApi 함수 분리 및 불필요한 파일 삭제#55

--- BE/src/stock/trade/history/stock-trade-history.service.ts
+++ BE/src/stock/trade/history/stock-trade-history.service.ts
@@ -3,7 +3,6 @@ import { Injectable, Logger } from '@nestjs/common';
 import { KoreaInvestmentService } from '../../../koreaInvestment/korea-investment.service';
 import { getHeader } from '../../../util/get-header';
 import { getFullURL } from '../../../util/get-full-URL';
-import { StockTradeHistoryQueryParameterDto } from './dto/stock-trade-history-query-parameter.dto';
 import { InquireCCNLApiResponse } from './interface/Inquire-ccnl.interface';
 import { StockTradeHistoryOutputDto } from './dto/stock-trade-history-output.dto';
 import { StockTradeHistoryDataDto } from './dto/stock-trade-history-data.dto';
@@ -23,11 +22,16 @@ export class StockTradeHistoryService {
    */
   async getStockTradeHistory(stockCode: string) {
     try {
-      const queryParams = new StockTradeHistoryQueryParameterDto();
-      queryParams.fid_cond_mrkt_div_code = 'J';
-      queryParams.fid_input_iscd = stockCode;
+      const queryParams = {
+        fid_cond_mrkt_div_code: 'J',
+        fid_input_iscd: stockCode,
+      };
 
-      const response = await this.requestApi(queryParams);
+      const response = await this.requestApi<InquireCCNLApiResponse>(
+        'FHKST01010300',
+        '/uapi/domestic-stock/v1/quotations/inquire-ccnl',
+        queryParams,
+      );
 
       return this.formatTradeHistoryData(response.output);
     } catch (error) {
@@ -36,38 +40,6 @@ export class StockTradeHistoryService {
         statusText: error.response?.statusText,
         data: error.response?.data,
         headers: error.response?.config?.headers, // 실제 요청 헤더
-        message: error.message,
-      });
-      throw error;
-    }
-  }
-
-  /**
-   * @private 한국투자 Open API - [국내주식] 기본시세 - 주식현재가 체결 호출 함수
-   * @param {StockTradeHistoryQueryParameterDto} queryParams - API 요청 시 필요한 쿼리 파라미터 DTO
-   * @returns - 주식현재가 체결 데이터
-   *
-   * @author uuuo3o
-   */
-  private async requestApi(queryParams: StockTradeHistoryQueryParameterDto) {
-    try {
-      const accessToken = await this.koreaInvetmentService.getAccessToken();
-      const headers = getHeader(accessToken, 'FHKST01010300');
-      const url = getFullURL('/uapi/domestic-stock/v1/quotations/inquire-ccnl');
-      const params = this.getTradeHistoryParams(queryParams);
-
-      const response = await axios.get<InquireCCNLApiResponse>(url, {
-        headers,
-        params,
-      });
-
-      return response.data;
-    } catch (error) {
-      this.logger.error('API Error Details:', {
-        status: error.response?.status,
-        statusText: error.response?.statusText,
-        data: error.response?.data,
-        headers: error.response?.config?.headers,
         message: error.message,
       });
       throw error;
@@ -95,16 +67,39 @@ export class StockTradeHistoryService {
   }
 
   /**
-   * @private 주식현재가 체결 요청을 위한 쿼리 파라미터 객체 생성 함수
-   * @param {StockTradeHistoryQueryParameterDto} params - API 요청에 필요한 쿼리 파라미터 DTO
-   * @returns - API 요청에 필요한 쿼리 파라미터 객체
+   * @private 한국투자 Open API - API 호출용 공통 함수
+   * @param {string} trId - API 호출에 사용할 tr_id
+   * @param {string} apiURL - API 호출에 사용할 URL
+   * @param {Record<string, string>} params - API 요청 시 필요한 쿼리 파라미터 DTO
+   * @returns - API 호출에 대한 응답 데이터
    *
    * @author uuuo3o
    */
-  private getTradeHistoryParams(params: StockTradeHistoryQueryParameterDto) {
-    return {
-      fid_cond_mrkt_div_code: params.fid_cond_mrkt_div_code,
-      fid_input_iscd: params.fid_input_iscd,
-    };
+  private async requestApi<T>(
+    trId: string,
+    apiURL: string,
+    params: Record<string, string>,
+  ): Promise<T> {
+    try {
+      const accessToken = await this.koreaInvetmentService.getAccessToken();
+      const headers = getHeader(accessToken, trId);
+      const url = getFullURL(apiURL);
+
+      const response = await axios.get<T>(url, {
+        headers,
+        params,
+      });
+
+      return response.data;
+    } catch (error) {
+      this.logger.error('API Error Details:', {
+        status: error.response?.status,
+        statusText: error.response?.statusText,
+        data: error.response?.data,
+        headers: error.response?.config?.headers,
+        message: error.message,
+      });
+      throw error;
+    }
   }
 }
