4a686ac957f9ed315da4d225e75dd99cfb412a33
JIN <o3ouuu@inu.ac.kr>
2024-11-13 20:16:30 +0900

    ✨ feat: 주식현재가 일자별 API 요청 로직 구현#55

--- BE/src/stock/trade/history/stock-trade-history.service.ts
+++ BE/src/stock/trade/history/stock-trade-history.service.ts
@@ -6,6 +6,9 @@ import { getFullURL } from '../../../util/get-full-URL';
 import { InquireCCNLApiResponse } from './interface/Inquire-ccnl.interface';
 import { TodayStockTradeHistoryOutputDto } from './dto/today-stock-trade-history-output.dto';
 import { TodayStockTradeHistoryDataDto } from './dto/today-stock-trade-history-data.dto';
+import { InquireDailyPriceApiResponse } from './interface/inquire-daily-price.interface';
+import { DailyStockTradeHistoryOutputDto } from './dto/daily-stock-trade-history-ouput.dto';
+import { DailyStockTradeHistoryDataDto } from './dto/daily-stock-trade-history-data.dto';
 
 @Injectable()
 export class StockTradeHistoryService {
@@ -65,6 +68,66 @@ export class StockTradeHistoryService {
       infoData.prdy_ctrt = info.prdy_ctrt;
 
       return infoData;
+    });
+  }
+
+  /**
+   * 특정 주식의 일자별 체결 데이터를 반환하는 함수
+   * @param {string} stockCode - 종목코드
+   * @returns - 특정 주식의 현재가 체결 데이터 객체 반환
+   *
+   * @author uuuo3o
+   */
+  async getDailyStockTradeHistory(stockCode: string) {
+    try {
+      const queryParams = {
+        fid_cond_mrkt_div_code: 'J',
+        fid_input_iscd: stockCode,
+        fid_period_div_code: 'D',
+        fid_org_adj_prc: '0',
+      };
+
+      const response = await this.requestApi<InquireDailyPriceApiResponse>(
+        'FHKST01010400',
+        '/uapi/domestic-stock/v1/quotations/inquire-daily-price',
+        queryParams,
+      );
+
+      return this.formatDailyStockTradeHistoryData(response.output);
+    } catch (error) {
+      this.logger.error('API Error Details:', {
+        status: error.response?.status,
+        statusText: error.response?.statusText,
+        data: error.response?.data,
+        headers: error.response?.config?.headers, // 실제 요청 헤더
+        message: error.message,
+      });
+      throw error;
+    }
+  }
+
+  /**
+   * @private API에서 받은 주식현재가 일자별 데이터를 필요한 정보로 정제하는 함수
+   * @param {DailyStockTradeHistoryOutputDto} datas - API 응답에서 받은 원시 데이터
+   * @returns - 필요한 정보만 추출한 데이터 배열
+   *
+   * @author uuuo3o
+   */
+  private formatDailyStockTradeHistoryData(
+    datas: DailyStockTradeHistoryOutputDto[],
+  ) {
+    return datas.map((data) => {
+      const historyData = new DailyStockTradeHistoryDataDto();
+      historyData.stck_bsop_date = data.stck_bsop_date;
+      historyData.stck_oprc = data.stck_oprc;
+      historyData.stck_hgpr = data.stck_hgpr;
+      historyData.stck_lwpr = data.stck_lwpr;
+      historyData.stck_clpr = data.stck_clpr;
+      historyData.acml_vol = data.acml_vol;
+      historyData.prdy_vrss_sign = data.prdy_vrss_sign;
+      historyData.prdy_ctrt = data.prdy_ctrt;
+
+      return historyData;
     });
   }
 
