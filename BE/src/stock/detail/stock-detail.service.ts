5f7d33c2842bdabe1027b9d84ac69c8c703852af
JIN <o3ouuu@inu.ac.kr>
2024-11-13 18:01:53 +0900

    ✨ feat: 주식 현재가 API 요청 로직 구현#54

--- BE/src/stock/detail/stock-detail.service.ts
+++ BE/src/stock/detail/stock-detail.service.ts
@@ -3,14 +3,71 @@ import { Injectable, Logger } from '@nestjs/common';
 import { KoreaInvestmentService } from '../../koreaInvestment/korea-investment.service';
 import { getHeader } from '../../util/get-header';
 import { getFullURL } from '../../util/get-full-URL';
-import { InquirePriceApiResponse } from './interface/stock-detail-chart.interface';
+import { InquirePriceChartApiResponse } from './interface/stock-detail-chart.interface';
 import { InquirePriceChartDataDto } from './dto/stock-detail-chart-data.dto';
+import {
+  InquirePriceApiResponse,
+  InquirePriceOutputData,
+} from './interface/stock-detail.interface';
+import { InquirePriceResponseDto } from './dto/stock-detail-response.dto';
 
 @Injectable()
 export class StockDetailService {
   private readonly logger = new Logger();
 
   constructor(private readonly koreaInvetmentService: KoreaInvestmentService) {}
+
+  /**
+   * 주식현재가 시세 데이터를 반환하는 함수
+   * @param {string} stockCode - 종목코드
+   * @returns - 주식현재가 시세 데이터 객체 반환
+   *
+   * @author uuuo3o
+   */
+  async getInquirePrice(stockCode: string) {
+    try {
+      const queryParams = {
+        fid_cond_mrkt_div_code: 'J',
+        fid_input_iscd: stockCode,
+      };
+
+      const response = await this.requestApi<InquirePriceApiResponse>(
+        'FHKST01010100',
+        '/uapi/domestic-stock/v1/quotations/inquire-price',
+        queryParams,
+      );
+
+      return this.formatStockData(response.output);
+    } catch (error) {
+      this.logger.error('API Error Details:', {
+        status: error.response?.status,
+        statusText: error.response?.statusText,
+        data: error.response?.data,
+        headers: error.response?.config?.headers, // 실제 요청 헤더
+        message: error.message,
+      });
+      throw error;
+    }
+  }
+
+  /**
+   * @private API에서 받은 주식현재가 시세 데이터를 필요한 정보로 정제하는 함수
+   * @param {InquirePriceOutputData} stock - API 응답에서 받은 원시 데이터
+   * @returns - 필요한 정보만 추출한 데이터 배열
+   *
+   * @author uuuo3o
+   */
+  private formatStockData(stock: InquirePriceOutputData) {
+    const stockData = new InquirePriceResponseDto();
+    stockData.stck_shrn_iscd = stock.stck_shrn_iscd;
+    stockData.stck_prpr = stock.stck_prpr;
+    stockData.prdy_vrss = stock.prdy_vrss;
+    stockData.prdy_vrss_sign = stock.prdy_vrss_sign;
+    stockData.prdy_ctrt = stock.prdy_ctrt;
+    stockData.hts_avls = stock.hts_avls;
+    stockData.per = stock.per;
+    return stockData;
+  }
 
   /**
    * 특정 주식의 기간별시세 데이터를 반환하는 함수
@@ -38,7 +95,7 @@ export class StockDetailService {
         fid_org_adj_prc: '0',
       };
 
-      const response = await this.requestApi<InquirePriceApiResponse>(
+      const response = await this.requestApi<InquirePriceChartApiResponse>(
         'FHKST03010100',
         '/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice',
         queryParams,
@@ -64,7 +121,7 @@ export class StockDetailService {
    *
    * @author uuuo3o
    */
-  private formatStockInquirePriceData(response: InquirePriceApiResponse) {
+  private formatStockInquirePriceData(response: InquirePriceChartApiResponse) {
     const { output2 } = response;
 
     return output2.map((info) => {
