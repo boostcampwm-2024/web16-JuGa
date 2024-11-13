c570788925dda60cee284976d3af036392e82b4b
jinddings <pjm2476@gmail.com>
2024-11-13 11:56:38 +0900

    📝 docs : swagger 문서 Parameter 필수 수정

--- BE/src/stock/list/dto/stock-list-response.dto.ts
+++ BE/src/stock/list/dto/stock-list-response.dto.ts
@@ -1,13 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class StockListResponseDto {
-  @ApiProperty({ example: '005930', description: '종목 코드' })
+  @ApiProperty({ example: '005930', description: '종목 코드', required: false })
   code: string;
 
-  @ApiProperty({ example: '삼성전자', description: '종목 이름' })
+  @ApiProperty({
+    example: '삼성전자',
+    description: '종목 이름',
+    required: false,
+  })
   name: string;
 
-  @ApiProperty({ example: 'KOSPI', description: '시장' })
+  @ApiProperty({ example: 'KOSPI', description: '시장', required: false })
   market: string;
 
   constructor(code: string, name: string, market: string) {
