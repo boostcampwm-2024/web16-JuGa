1fa482c453ef1ab30b3d935e3666c66a4ab52400
Seo San <dannysir@naver.com>
2024-11-14 12:56:38 +0900

    💄 design: 검색 결과가 없을 때 화면 추가.

--- FE/src/components/Search/SearchList.tsx
+++ FE/src/components/Search/SearchList.tsx
@@ -1,11 +1,26 @@
 import SearchCard from './SearchCard.tsx';
 import { searchDataType } from './searchDataType.ts';
+import Lottie from 'lottie-react';
+import noResultAnimation from 'assets/noResultAnimation.json';
 
 type SearchListProps = {
   searchData: searchDataType[];
 };
 
 export default function SearchList({ searchData }: SearchListProps) {
+  if (searchData.length === 0) {
+    return (
+      <div
+        className={'flex h-[320px] flex-col items-center justify-center gap-5'}
+      >
+        <Lottie animationData={noResultAnimation} className={'h-[200px]'} />
+        <div className={'font-bold text-juga-grayscale-black'}>
+          검색 결과가 없습니다.
+        </div>
+      </div>
+    );
+  }
+
   return (
     <ul>
       {searchData.map((data, index) => (
