03ac949c44a004c7e360f6c2405203b9a4ee3f33
JIN <o3ouuu@inu.ac.kr>
2024-11-18 15:54:03 +0900

    ✨ feat: 모의 도메인용 함수 추가#127

--- BE/src/util/get-full-URL.ts
+++ BE/src/util/get-full-URL.ts
@@ -1,3 +1,7 @@
 export const getFullURL = (url: string) => {
   return `${process.env.KOREA_INVESTMENT_BASE_URL}${url}`;
 };
+
+export const getFullTestURL = (url: string) => {
+  return `${process.env.KOREA_INVESTMENT_TEST_BASE_URL}${url}`;
+};
