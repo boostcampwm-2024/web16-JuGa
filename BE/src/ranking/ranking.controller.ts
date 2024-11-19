b338c10bbe5995262412f492e537a4decd994618
jinddings <pjm2476@gmail.com>
2024-11-19 19:11:27 +0900

    📝 docs : api swagger 설명 추가(#124)

--- BE/src/ranking/ranking.controller.ts
+++ BE/src/ranking/ranking.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { OptionalAuthGuard } from 'src/auth/optional-auth-guard';
 import { RankingService } from './ranking.service';
+import { RankingResponseDto } from './dto/ranking-response.dto';
 
 @Controller('/api/ranking')
+@ApiTags('랭킹 API')
 export class RankingController {
   constructor(private readonly rankingService: RankingService) {}
 
   @ApiOperation({ summary: '랭킹 조회' })
+  @ApiResponse({
+    status: 200,
+    description: '랭킹 조회 성공',
+    type: RankingResponseDto,
+  })
   @Get()
   @UseGuards(OptionalAuthGuard)
   async getRanking(@Req() req: Request) {
