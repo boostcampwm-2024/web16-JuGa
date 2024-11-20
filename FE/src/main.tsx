8e7448cfb47f16cb89d1fafa85e85ba80535ad22
dongree <dongwoo0307@naver.com>
2024-11-20 21:00:14 +0900

    ✨ feat: error boundary 설정

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -2,14 +2,32 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryErrorResetBoundary,
+} from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import FallbackUI from 'components/FallbackUI.tsx';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      useErrorBoundary: true,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary onReset={reset} FallbackComponent={FallbackUI}>
+            <App />
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </QueryClientProvider>
   </StrictMode>,
 );
