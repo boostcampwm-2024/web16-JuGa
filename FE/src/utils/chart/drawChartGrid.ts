7968b1a03826ca2b8a87dfb224084fa689836bb6
Seo San <dannysir@naver.com>
2024-11-20 18:20:43 +0900

    ✨ feat: X축 그리드 생성 #123

--- FE/src/utils/chart/drawChartGrid.ts
+++ FE/src/utils/chart/drawChartGrid.ts
@@ -1,5 +1,5 @@
 import { Padding, StockChartUnit } from '../../types.ts';
-import { makeYLabels } from './makeLabels.ts';
+import { makeXLabels, makeYLabels } from './makeLabels.ts';
 
 export const drawChartGrid = (
   upperChartCtx: CanvasRenderingContext2D,
@@ -13,6 +13,37 @@ export const drawChartGrid = (
   data: StockChartUnit[],
   padding: Padding,
 ) => {
+  const xAxisLabels = makeXLabels(data);
+  const barWidth = Math.floor(lowerChartWidth / data.length);
+  lowerChartCtx.beginPath();
+  upperChartCtx.beginPath();
+  data.forEach((item, i) => {
+    if (xAxisLabels.includes(item.stck_bsop_date) || i === data.length - 1) {
+      lowerChartCtx.moveTo(
+        padding.left + (lowerChartWidth * i) / (data.length - 1) + barWidth / 2,
+        0,
+      );
+      lowerChartCtx.lineTo(
+        padding.left + (lowerChartWidth * i) / (data.length - 1) + barWidth / 2,
+        lowerChartHeight + padding.bottom,
+      );
+      upperChartCtx.moveTo(
+        padding.left + (upperChartWidth * i) / (data.length - 1) + barWidth / 2,
+        padding.top,
+      );
+      upperChartCtx.lineTo(
+        padding.left + (upperChartWidth * i) / (data.length - 1) + barWidth / 2,
+        upperChartHeight + padding.top + padding.bottom,
+      );
+    }
+  });
+  lowerChartCtx.strokeStyle = '#D2DAE0';
+  lowerChartCtx.lineWidth = 1;
+  lowerChartCtx.stroke();
+  upperChartCtx.strokeStyle = '#D2DAE0';
+  upperChartCtx.lineWidth = 1;
+  upperChartCtx.stroke();
+
   const volumes = data.map((d) => +d.acml_vol);
   const volumeMax = Math.round(Math.max(...volumes) * 1.2);
   const volumeMin = Math.round(Math.min(...volumes) * 0.8);
@@ -38,22 +69,22 @@ export const drawChartGrid = (
   );
   lowerChartCtx.lineTo(lowerChartWidth + padding.left + padding.right, 0);
   lowerChartCtx.strokeStyle = '#D2DAE0';
-  lowerChartCtx.lineWidth = 2;
+  lowerChartCtx.lineWidth = 1;
   lowerChartCtx.stroke();
 
   const values = data
     .map((d) => [+d.stck_hgpr, +d.stck_lwpr, +d.stck_clpr, +d.stck_oprc])
     .flat();
-  const yMax = Math.round(Math.max(...values) * (1 + 0.1));
-  const yMin = Math.round(Math.min(...values) * (1 - 0.1));
+  const valueMax = Math.round(Math.max(...values) * (1 + 0.1));
+  const valueMin = Math.round(Math.min(...values) * (1 - 0.1));
 
-  const labels = makeYLabels(yMax, yMin, upperLabelsNum);
+  const upperLabels = makeYLabels(valueMax, valueMin, upperLabelsNum);
   upperChartCtx.beginPath();
-  labels.forEach((label) => {
+  upperLabels.forEach((label) => {
     const yPos =
       padding.top +
       upperChartHeight -
-      ((label - yMin) / (yMax - yMin)) * upperChartHeight;
+      ((label - valueMin) / (valueMax - valueMin)) * upperChartHeight;
     upperChartCtx.moveTo(0, yPos);
     upperChartCtx.lineTo(upperChartWidth + padding.left + padding.right, yPos);
   });
@@ -66,6 +97,6 @@ export const drawChartGrid = (
     padding.top,
   );
   upperChartCtx.strokeStyle = '#D2DAE0';
-  upperChartCtx.lineWidth = 2;
+  upperChartCtx.lineWidth = 1;
   upperChartCtx.stroke();
 };
