ae4401500c761c7ca5e490b05344e50e9dd3dc55
anjdydhody <a01066444534@gmail.com>
2024-11-25 20:00:28 +0900

    ✨ feat: 주식 즐겨찾기 등록 API 구현 #177

--- BE/src/stock/bookmark/stock-bookmark.controller.ts
+++ BE/src/stock/bookmark/stock-bookmark.controller.ts
@@ -1,9 +1,34 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Request } from 'express';
 import { StockBookmarkService } from './stock-bookmark.service';
+import { JwtAuthGuard } from '../../auth/jwt-auth-guard';
 
 @Controller('/api/stocks/bookmark')
 @ApiTags('주식 즐겨찾기 API')
 export class StockBookmarkController {
   constructor(private readonly stockBookmarkService: StockBookmarkService) {}
+
+  @Post('/:stockCode')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '종목 즐겨찾기 등록 API' })
+  @ApiResponse({
+    status: 201,
+    description: '종목 즐겨찾기 등록 성공',
+  })
+  async postBookmark(
+    @Req() request: Request,
+    @Param('stockCode') stockCode: string,
+  ) {
+    await this.stockBookmarkService.registerBookmark(
+      parseInt(request.user.userId, 10),
+      stockCode,
+    );
+  }
 }
--- BE/src/stock/bookmark/stock-bookmark.service.ts
+++ BE/src/stock/bookmark/stock-bookmark.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { StockBookmarkRepository } from './stock-bookmark.repository';
 
 @Injectable()
@@ -6,4 +6,20 @@ export class StockBookmarkService {
   constructor(
     private readonly stockBookmarkRepository: StockBookmarkRepository,
   ) {}
+
+  async registerBookmark(userId, stockCode) {
+    if (
+      await this.stockBookmarkRepository.existsBy({
+        user_id: userId,
+        stock_code: stockCode,
+      })
+    )
+      throw new BadRequestException('이미 등록된 즐겨찾기입니다.');
+
+    const bookmark = this.stockBookmarkRepository.create({
+      user_id: userId,
+      stock_code: stockCode,
+    });
+    await this.stockBookmarkRepository.insert(bookmark);
+  }
 }
