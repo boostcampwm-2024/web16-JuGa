82673026180550c8c420089c842985a31fbd0fd8
Seo San <dannysir@naver.com>
2024-11-26 18:03:26 +0900

    ✨ feat: 뉴스 관련 컴포넌트 제작 #188 이미지 사용 불가로 인한 뉴스 관련 컴포넌트 디자인 수정.

---
+++ FE/src/components/News/Card.tsx
@@ -0,0 +1,30 @@
+import { NewsMockDataType } from './newsMockData.ts';
+
+type CardWithImageProps = {
+  data: NewsMockDataType;
+};
+export default function Card({ data }: CardWithImageProps) {
+  return (
+    <div className='flex cursor-pointer flex-col rounded-lg border p-4 transition-all hover:bg-gray-50'>
+      <div className={'mb-2 flex flex-row items-center justify-between'}>
+        <div className={'flex flex-row items-center gap-3'}>
+          <span className='rounded-full bg-blue-50 px-2 py-0.5 text-xs text-blue-600'>
+            증권
+          </span>
+          <h3 className='line-clamp-1 text-left text-base font-medium'>
+            {data.title}
+          </h3>
+        </div>
+        <span className={'w-fit text-sm text-gray-500'}>{data.date}</span>
+      </div>
+      <div className='flex items-center justify-between gap-4'>
+        <p className='line-clamp-1 flex-1 text-left text-sm text-gray-500'>
+          {data.img}
+        </p>
+        <span className='whitespace-nowrap text-sm text-gray-500'>
+          {data.publisher}
+        </span>
+      </div>
+    </div>
+  );
+}
