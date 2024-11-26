e33fce683ac885669483d02d714743c7bcf1823b
JIN <o3ouuu@inu.ac.kr>
2024-11-26 20:17:44 +0900

    ✨ feat: 네이버 검색 api를 활용해 검색어 별로 5개의 뉴스 정보를 받아올 수 있도록 구현#181

--- BE/src/news/news.service.ts
+++ BE/src/news/news.service.ts
@@ -1,43 +1,34 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
 import { Injectable } from '@nestjs/common';
-import { NewsValue } from './interface/news-value.interface';
+import { NaverApiDomianService } from './naver-api-domian.service';
+import { NewsApiResponse } from './interface/news-value.interface';
+import { NewsDataOutputDto } from './dto/news-data-output.dto';
+import { NewsResponseDto } from './dto/news-response.dto';
 
 @Injectable()
 export class NewsService {
-  private readonly URL = 'https://news.naver.com/breakingnews/section/101/258';
+  constructor(private readonly naverApiDomainService: NaverApiDomianService) {}
 
-  /**
-   * 이거 자체를 1시간에 한번 뭐 이렇게 반복하게 해서 DB에 저장하고
-   * (중복 데이터도 있을테니 계속 쌓이게말고 싹 다 지우고 업데이트 한다거나.. 그런 식으로?)
-   * 클라이언트 측에서는 그냥 db에 있는 값 꺼내오는거로
-   */
-  async crawlNews() {
-    const response = await axios.get(this.URL);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const $ = cheerio.load(response.data);
-    const newsArray: NewsValue[] = [];
+  async getNewsDataByQuery(value: string) {
+    const queryParams = {
+      query: value,
+    };
 
-    $('.sa_item').each((_, element) => {
-      const $item = $(element);
+    const response =
+      await this.naverApiDomainService.requestApi<NewsApiResponse>(queryParams);
+    return this.formatNewsData(value, response.items);
+  }
 
-      const imageUrl = $item.find('.sa_thumb_inner img').attr('data-src') || '';
-      const newsUrl = $item.find('a.sa_text_title').attr('href');
-      const title = $item.find('a.sa_text_title').text().trim();
-      const description = $item.find('.sa_text_lede').text().trim();
-      const press = $item.find('.sa_text_press').text().trim();
-      const time = $item.find('.sa_text_datetime').text().trim();
+  private formatNewsData(query: string, items: NewsDataOutputDto[]) {
+    return items.slice(0, 5).map((item) => {
+      const result = new NewsResponseDto();
 
-      newsArray.push({
-        imageUrl,
-        newsUrl,
-        title,
-        description,
-        press,
-        time,
-      });
+      result.title = item.title.replace(/<\/?b>/g, '');
+      result.description = item.description.replace(/<\/?b>/g, '');
+      result.originallink = item.originallink;
+      result.pubDate = item.pubDate;
+      result.query = query;
+
+      return result;
     });
-
-    return [newsArray.length, newsArray];
   }
 }
