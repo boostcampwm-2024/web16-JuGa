5606380e084d1b22b64fddb6b2bd9e8b63ccb8aa
JIN <o3ouuu@inu.ac.kr>
2024-11-26 19:43:18 +0900

    ✨ feat: 뉴스 API에 사용할 DTO, interface 생성#181

--- BE/src/news/interface/news-value.interface.ts
+++ BE/src/news/interface/news-value.interface.ts
@@ -1,8 +1,9 @@
-export interface NewsValue {
-  imageUrl: string;
-  newsUrl: string;
-  title: string;
-  description: string;
-  press: string;
-  time: string;
+import { NewsDataOutputDto } from '../dto/news-data-output.dto';
+
+export interface NewsApiResponse {
+  lastBuildDate: string;
+  total: number;
+  start: number;
+  display: number;
+  item: NewsDataOutputDto[];
 }
