22b0e31a31e7fd4d6dcdbba5507003df04b33b88
JIN <o3ouuu@inu.ac.kr>
2024-11-27 13:40:26 +0900

    ✨ feat: 필요한 값만 반환할 수 있게 기능 추가

--- BE/src/news/news.controller.ts
+++ BE/src/news/news.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { NewsService } from './news.service';
-import { NewsDatabaseResponseDto } from './dto/news-database-response.dto';
+import { NewsResponseDto } from './dto/news-response.dto';
 
 @ApiTags('뉴스 API')
 @Controller('/api/news')
@@ -12,7 +12,7 @@ export class NewsController {
   @ApiResponse({
     status: 200,
     description: '뉴스 조회 성공',
-    type: [NewsDatabaseResponseDto],
+    type: [NewsResponseDto],
   })
   async getNews() {
     return this.newsService.getNews();
