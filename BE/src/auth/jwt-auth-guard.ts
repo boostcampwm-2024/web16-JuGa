297cd929aede344b72d6dc92a75166ffda1d55be
jinddings <pjm2476@gmail.com>
2024-11-27 11:14:38 +0900

    🚑 !HOTFIX : jwt auth guard 로 인해 발생하는 문제 해결 및 jwt strategy 수정

--- BE/src/auth/jwt-auth-guard.ts
+++ BE/src/auth/jwt-auth-guard.ts
@@ -1,14 +1,5 @@
 import { AuthGuard } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(['jwt', 'kakao']) {
-  handleRequest(err, user) {
-    if (user) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return user;
-    }
-
-    throw err || new UnauthorizedException();
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
