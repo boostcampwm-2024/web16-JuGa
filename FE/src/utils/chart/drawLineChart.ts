8d6e8e2f3010a2bd35b30e73ad628fa0fdd45963
Seo San <dannysir@naver.com>
2024-11-27 17:11:37 +0900

    ✨ feat: 이동 평균선 추가 작업 & 오타 정리.

--- FE/src/utils/chart/drawLineChart.ts
+++ FE/src/utils/chart/drawLineChart.ts
@@ -9,27 +9,35 @@ export function drawLineChart(
   height: number,
   padding: Padding,
   weight: number = 0,
-  lineWidth: number = 1,
+  lineWidth: number = 4,
 ) {
   if (data.length === 0) return;
 
   ctx.beginPath();
 
   const n = data.length;
-  const yMax = Math.round(
-    Math.max(...data.map((d: StockChartUnit) => +d.stck_oprc)) * (1 + weight),
-  );
-  const yMin = Math.round(
-    Math.min(...data.map((d: StockChartUnit) => +d.stck_oprc)) * (1 - weight),
-  );
+  const gap = Math.floor(width / n);
+
+  const values = data
+    .map((d) => [
+      +d.stck_hgpr,
+      +d.stck_lwpr,
+      +d.stck_clpr,
+      +d.stck_oprc,
+      Math.floor(+d.mov_avg_5),
+      Math.floor(+d.mov_avg_20),
+    ])
+    .flat();
+  const yMax = Math.round(Math.max(...values) * (1 + weight));
+  const yMin = Math.round(Math.min(...values) * (1 - weight));
 
   data.forEach((e, i) => {
-    const cx = x + padding.left + (width * i) / (n - 1);
+    const cx = x + padding.left + (width * i) / (n - 1) + gap / 2;
     const cy =
       y +
       padding.top +
       height -
-      (height * (+e.stck_oprc - yMin)) / (yMax - yMin);
+      (height * (+e.mov_avg_5 - yMin)) / (yMax - yMin);
 
     if (i === 0) {
       ctx.moveTo(cx, cy);
@@ -37,7 +45,7 @@ export function drawLineChart(
       ctx.lineTo(cx, cy);
     }
   });
-
+  ctx.strokeStyle = '#000';
   ctx.lineWidth = lineWidth;
   ctx.stroke();
 }
