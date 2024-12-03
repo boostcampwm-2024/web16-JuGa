f7596e39c65ea4e60f074c8728e375a9f13dece3
dongree <dongwoo0307@naver.com>
2024-12-04 00:08:51 +0900

    ➕ add: 마이페이지 주문 요청 현황 list 클릭시 해당 주식 상세 페이지 들어갈 수 있도록 추가

--- FE/src/components/Mypage/Order.tsx
+++ FE/src/components/Mypage/Order.tsx
@@ -2,6 +2,7 @@ import useOrders from 'hooks/useOrder';
 import useOrderCancelAlertModalStore from 'store/useOrderCancleAlertModalStore';
 import CancelAlertModal from './CancelAlertModal.tsx';
 import { formatTimestamp } from 'utils/format';
+import { useNavigate } from 'react-router-dom';
 
 export default function Order() {
   const { orderQuery, removeOrder } = useOrders();
@@ -9,10 +10,16 @@ export default function Order() {
   const { data } = orderQuery;
   const { isOpen, open } = useOrderCancelAlertModalStore();
 
+  const navigation = useNavigate();
+
+  const handleClick = (code: string) => {
+    navigation(`/stocks/${code}`);
+  };
+
   return (
     <div className='mx-auto flex min-h-[500px] w-full flex-col rounded-md bg-white p-4 shadow-md'>
-      <div className='flex border-b pb-2 text-sm font-bold'>
-        <p className='w-1/3 truncate text-left'>종목</p>
+      <div className='flex pb-2 text-sm font-bold border-b'>
+        <p className='w-1/3 text-left truncate'>종목</p>
         <p className='w-1/4 text-center'>요청 유형</p>
         <p className='w-1/4 text-center'>수량</p>
         <p className='w-1/4 text-center'>요청 가격</p>
@@ -20,7 +27,7 @@ export default function Order() {
         <p className='w-1/6 text-right'></p>
       </div>
 
-      <ul className='flex flex-col divide-y text-sm'>
+      <ul className='flex flex-col text-sm divide-y'>
         {data?.map((order) => {
           const {
             id,
@@ -33,8 +40,12 @@ export default function Order() {
           } = order;
 
           return (
-            <li className='flex py-2' key={id}>
-              <div className='flex w-1/3 gap-2 truncate text-left'>
+            <li
+              className='flex py-2 transition-colors hover:cursor-pointer hover:bg-gray-50'
+              key={id}
+              onClick={() => handleClick(stock_code)}
+            >
+              <div className='flex w-1/3 gap-2 text-left truncate'>
                 <p className='font-semibold'>{stock_name}</p>
                 <p className='text-gray-500'>{stock_code}</p>
               </div>
@@ -47,15 +58,15 @@ export default function Order() {
               >
                 {trade_type === 'BUY' ? '매수' : '매도'}
               </p>
-              <p className='w-1/4 truncate text-center'>{amount}</p>
+              <p className='w-1/4 text-center truncate'>{amount}</p>
               <p className='w-1/4 text-center'>{price.toLocaleString()}원</p>
-              <p className='w-1/4 truncate text-right'>
+              <p className='w-1/4 text-right truncate'>
                 {formatTimestamp(created_at)}
               </p>
               <p className='w-1/6 text-right'>
                 <button
                   onClick={() => open(order, () => removeOrder.mutate(id))}
-                  className='rounded-lg bg-juga-grayscale-500 px-2 py-1 text-xs text-white transition hover:bg-juga-grayscale-black'
+                  className='px-2 py-1 text-xs text-white transition rounded-lg bg-juga-grayscale-500 hover:bg-juga-grayscale-black'
                 >
                   취소
                 </button>
