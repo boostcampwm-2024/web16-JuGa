b098e53d3da37825f91eaa47f62c255889d9a250
dongree <dongwoo0307@naver.com>
2024-12-03 23:25:12 +0900

    ⚙️ chore: 불필요한 코드 제거

--- FE/src/store/useAuthStore.ts
+++ FE/src/store/useAuthStore.ts
@@ -1,24 +1,14 @@
 import { create } from 'zustand';
 
 type AuthStore = {
-  accessToken: string | null;
   isLogin: boolean;
-  setAccessToken: (token: string) => void;
   setIsLogin: (isLogin: boolean) => void;
-  resetToken: () => void;
 };
 
 const useAuthStore = create<AuthStore>((set) => ({
-  accessToken: null,
   isLogin: false,
   setIsLogin: (isLogin: boolean) => {
     set({ isLogin });
-  },
-  setAccessToken: (token: string) => {
-    set({ accessToken: token, isLogin: token !== null });
-  },
-  resetToken: () => {
-    set({ accessToken: null, isLogin: false });
   },
 }));
 
